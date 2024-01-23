18a7f1ae50f0e5adf1d65e3932bd0a0d0cc7d9a9
Agrima <agrimafeb02@gmail.com>
2024-01-23 12:36:44 +0530

    completed in flask

--- src/resources/user.py
+++ src/resources/user.py
@@ -19,10 +19,10 @@ class UserLogin(MethodView):
         try:
             user = login(user_data['username'], user_data['password'])
             print(user)
-            get_role = Role.get_role(user[0][0])
             if  user is False:
                 abort(401, message = "Username or password does not exist")
             else:
+                get_role = Role.get_role(user[0][0])
                 access_token = create_access_token(identity=user[2], additional_claims={"role": get_role})
                 return {
                     "access_token": access_token,
@@ -43,11 +43,11 @@ class UserRegistration(MethodView):
             if not data:
                 abort(500, message = "Server error")
             elif data:
-                # send_simple_message(
-                #     'shreyansh.brbd@gmail.com', 
-                #     'Registered',
-                #     f'You have been registered with username {user_data["username"]}'
-                # )
+                send_simple_message(
+                    'shreyansh.brbd@gmail.com', 
+                    'Registered',
+                    f'You have been registered with username {user_data["username"]}'
+                )
                 return {
                     "ID": data[0],
                     "name": data[1]
